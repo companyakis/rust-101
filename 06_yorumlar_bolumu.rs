a90a2c44d03169c98745d6b7876b1150d258f6f5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-04 10:50:36 +0300

    Create 06_yorumlar_bolumu.rs

---
+++ 06_yorumlar_bolumu.rs
@@ -0,0 +1,20 @@
+// satır yorumu; bu yorum bu satırındır
+
+//yeni yorum burada
+
+//açıklama eklemek için veya
+
+//şimdilik çalıştırmak istemediğiniz kodlar için kullanabilirsiniz
+
+//-------->yeni konu aşağıda--------------
+
+/* ... */
+
+/*
+
+Çok satırlı yorum yapmak istersek, ne olacak?
+İşte, anlık olarak bunu yapıyoruz.
+Mustafa Buyukdereli Mayıs 2023'te bunları buraya ekledi.
+Rust dilinin DeFi - Merkeziyetsiz Finans için ana dil olacağına inanıyorum.
+
+*/
