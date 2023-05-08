b4fd4c855874736dffd631ce1482325c7e56f252
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-08 18:25:18 +0300

    Create 08_veri_turleri_bir.rs

---
+++ 08_veri_turleri_bir.rs
@@ -0,0 +1,25 @@
+//örtülü değişken tanımlama, implicit definition
+//rust değişkenin türünü anlayabilir diyebiliriz
+
+// let degisken_adi = deger;
+
+// -----------------------------------------
+
+//açıkça değişken tanımlama, explicit definition
+//değişken için tür açıkça belirtilir
+
+//let degisken_adi:veri_turu = deger;
+
+// -------------------------------------------
+
+/*
+scalar veri türleri: tek bir veri tutarlar
+
+=> integers, floats, boolean, character
+
+compound veri türleri: birden çok veri tek bir değişkende tutulur
+
+örnek: alınacaklar için alışveriş listesi oluşturma gibi...
+
+=>array, tuple
+*/
