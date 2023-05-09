7ccc20bfa23cd659a2d4abbc69c1c87d6882103c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-09 10:28:13 +0300

    Create 11_veri_turleri_dort.rs

---
+++ 11_veri_turleri_dort.rs
@@ -0,0 +1,21 @@
+//character ve string veri türleri
+
+//character yalnızca bir char değer, örneğin bir harf ya da sayı gibi
+// tek tırnakla '' kullanmalıyız!
+
+let a_char = 'a';
+
+//explicit definition da olanaklıdır
+
+let my_char:char = 'm';
+
+//string ise ardışık karakterleri içeren veri türü diyebiliriz
+//çift tırnakla kullanmalıyız;
+
+let adim = "Mustafa";
+
+//string konusu rust dilinde biraz karmaşık, ayrıntılar ileri konularda...
+
+//explicit definition da olanaklıdır, &str kullanımı
+
+let soyadim:&str = "Büyükdereli";
