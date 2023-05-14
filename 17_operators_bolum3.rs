e07b46546c2a261d8b86d016277992f72cd49161
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-14 06:31:52 +0300

    Create 17_operators_bolum3.rs

---
+++ 17_operators_bolum3.rs
@@ -0,0 +1,29 @@
+//karşılaştırma işlemcileri, comparison operators
+// <, >, <=, >=, ==, !=
+// == eşitttir ve != eşit değil
+//true ya da false dönüş olur
+  
+fn main() {
+    let x = 5;
+    let y = 10;
+    println!("x > y => {}", x > y); 
+    println!("x < y => {}", x < y); 
+    println!("x >= y => {}", x >= y);
+    println!("x <= y => {}", x <= y);
+    println!("x == y => {}", x == y);
+    println!("x != y => {}", x != y);
+}
+                                    
+/*
+işlem sonuçları
+                                   
+x > y => false
+x < y => true
+x >= y => false
+x <= y => true
+x == y => false
+x != y => true
+  
+*/
+
+  
