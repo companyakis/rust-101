8a7cf166346d07210898fab6491580fb1eb09d74
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-14 06:12:21 +0300

    Create 15_operators_bolum1.rs

---
+++ 15_operators_bolum1.rs
@@ -0,0 +1,26 @@
+// arithmetic operators
+// + - * / ve % , bildiğimiz işlem uygulayıcıları
+
+fn main() {
+    let x = 5;
+    let y = 2;
+    
+    println!("x = {} ve y = {}", x , y);
+    println!("Topla => x + y = {}", x + y);
+    println!("Çıkar => x - y = {}", x - y);
+    println!("Çarp => x * y = {}", x * y);
+    println!("Tam Böl => x / y = {}", x / y);
+    println!("Bölümden Kalanı Bul (Mod Al)=> {}", x % y);
+}
+
+/*
+Sonuçlar aşağıda: 
+
+x = 5 ve y = 2
+Topla => x + y = 7
+Çıkar => x - y = 3
+Çarp => x * y = 10
+Tam Böl => x / y = 2
+Bölümden Kalanı Bul (Mod Al)=> 1
+
+*/
